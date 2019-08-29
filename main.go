b8b6cc8a9b97b87130f15f197e4c3dc29e74cbb3
khomdrad <khomdradboon@gmail.com>
2019-08-29 17:52:10 +0700

    Added helloWorld route

--- main.go
+++ main.go
@@ -35,6 +35,10 @@ func getAddressBookAll(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(addBook) // (1)
 }
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "HelloWorld!")
+}
+
 func errorHandler(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -48,6 +52,7 @@ func main() {
 	fmt.Println("Using port:8080")
 
 	http.HandleFunc("/getAddress", getAddressBookAll)
+	http.HandleFunc("/helloWorld", helloWorld)
 	http.HandleFunc("/", homePage)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Print(err)
